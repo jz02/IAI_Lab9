f9278268b62ef1e4f4dd786111f0e4486778701a
jz02 <33122@student.pwsztar.edu.pl>
2020-12-16 23:51:01 +0100

    l9/l10

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+function test(a, b)
+{   return a + b;
+
+}
+var wynik = test ( a: 4, b: 5);
+console.log(wynik);
+*/
+function test(a, b) {
+    return a + b;
+}
+var wynik = test(4, 5);
+console.log(wynik);
---
+++ script.ts
@@ -0,0 +1,17 @@
+/*
+function test(a, b)
+{   return a + b;
+
+}
+var wynik = test ( a: 4, b: 5);
+console.log(wynik);
+*/
+
+function test(a: number, b: number): number
+{
+return a + b;
+}
+
+let wynik: number = test (4, 5);
+console.log(wynik);
+
