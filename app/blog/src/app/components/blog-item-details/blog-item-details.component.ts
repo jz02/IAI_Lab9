81dcd8920f6e2512146e0a65191e14af6101f00e
jz02 <33122@student.pwsztar.edu.pl>
2021-01-13 23:11:41 +0100

    Lab 11

--- app/blog/src/app/components/blog-item-details/blog-item-details.component.ts
+++ app/blog/src/app/components/blog-item-details/blog-item-details.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+
+import {Component, OnInit} from '@angular/core';
+import {DataService} from 'C:/Users/root/Desktop/11/IAI_Lab9/app/blog/src/app/services/data.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
-  selector: 'blog-item-details',
+  selector: 'app-blog-item-details',
   templateUrl: './blog-item-details.component.html',
   styleUrls: ['./blog-item-details.component.css']
 })
 export class BlogItemDetailsComponent implements OnInit {
 
-  constructor() { }
+  public image = '';
+  public text: string;
+  public id: number;
 
-  ngOnInit(): void {
+  constructor(private dataService: DataService, private route: ActivatedRoute) {
   }
 
+  ngOnInit() {
+	let id: string;
+	this.route.paramMap
+  	   .subscribe(params => {
+    	   id = params.get('id');
+  	});
+	if (id) {
+  	   this.dataService.getById(id).subscribe(res => {
+    	   this.image = res['image'];
+    	   this.text = res['text'];
+  	});
+	} else {
+  	   this.id = 1;
+	}
+  }
 }
+
